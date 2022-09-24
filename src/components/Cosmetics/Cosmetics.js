4271a31ac6b42bf2018ae39cce94967fe01e885e
faizulislamfair <faizulislamfair@gmail.com>
2022-09-24 09:57:24 +0600

    data generation system 3, through hooks

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,34 +1,16 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
-    const cosmetics = [
-        {
-            "id": "632e7dc1eade9c8f9a9bd973",
-            "price": 575,
-            "name": "Claudette Craft"
-        },
-        {
-            "id": "632e7dc1c6531727b800346e",
-            "price": 75,
-            "name": "Nielsen Pickett"
-        },
-        {
-            "id": "632e7dc156c16d318d44c944",
-            "price": 436,
-            "name": "Savage Miranda"
-        },
-        {
-            "id": "632e7dc1489af49cf901c26d",
-            "price": 578,
-            "name": "Madeline Singleton"
-        },
-        {
-            "id": "632e7dc1c08374aebb77da90",
-            "price": 241,
-            "name": "Brown Mcneil"
-        }
-    ]
+    const [cosmetics, setCosmetics] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setCosmetics(data));
+    }, [])
 
     return (
         <div>

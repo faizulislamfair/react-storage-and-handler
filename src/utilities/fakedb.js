d0da64a13ada092f7abc27dc4aafac97c3ee0f09
faizulislamfair <faizulislamfair@gmail.com>
2022-09-24 18:24:02 +0600

    multiple data in an object with local storage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,13 +1,29 @@
 // use localstorage to manage cart data
 
 const addToDb = id => {
-    const quantity = localStorage.getItem(id);
-    if (quantity) {
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
+    let shoppingCart;
+
+
+    // get the shopping cart
+    const storedCart = localStorage.getItem('shopping-cart');
+    if (storedCart) {
+        shoppingCart = JSON.parse(storedCart);
     } else {
-        localStorage.setItem(id, 1);
+        shoppingCart = {};
     }
+
+
+    // add quantity
+    const quantity = shoppingCart[id];
+    if (quantity) {
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    } else {
+        shoppingCart[id] = 1;
+    }
+
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+
 
 }
 
